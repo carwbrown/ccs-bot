d5d7184151f49d1f76932cfeb01429325c22de6b
carwbrown <carwbrown@google.com>
2021-05-09 22:42:53 -0400

    add the purge channel function

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
+  if (msg.content === "!ping") {
     msg.reply("Pong! test");
   }
-  if (msg.content === "purgeChannel") {
+  if (msg.content.startsWith === "!purge") {
     purgeChannel(msg);
   }
 });
--- purgeChannel.js
+++ purgeChannel.js
@@ -1,3 +1,15 @@
+async function clear(msg, generalChannel) {
+  console.log("deleting messages");
+  msg.delete();
+  const fetched = await generalChannel.fetchMessages({ limit: 99 });
+  generalChannel.bulkDelete(fetched);
+}
+
 export function purgeChannel(msg) {
+  const args = msg.content.split(/ +/);
+  console.log("args: ", args);
+  const generalChannel = bot.channels.find("name", "general");
+  console.log("generalChannel: ", generalChannel);
+  clear(msg, generalChannel);
   msg.reply("purge");
 }
