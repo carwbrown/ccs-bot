8ee3717b7f5b41a2806c51170837966e1df1c7ce
carwbrown <carwbrown@google.com>
2021-05-09 23:18:03 -0400

    prettier

--- purgeChannel.js
+++ purgeChannel.js
@@ -8,13 +8,13 @@ export function purgeChannel(client, msg) {
   const args = msg.content.split(/ +/);
   console.log("args: ", args);
 
-  const generalChannel = msg.guild.channels.cache.find(channel => {
-    return channel.name === args[1]
+  const generalChannel = msg.guild.channels.cache.find((channel) => {
+    return channel.name === args[1];
   });
-  if(!generalChannel) return;
+  if (!generalChannel) return;
   const generalObj = client.channels.cache.get(generalChannel.id);
 
   console.log("generalObj: ", generalObj);
   clear(msg, generalObj);
-  msg.author.send(`${generalChannel.name} channel has been cleaned out`}
+  msg.author.send(`${generalChannel.name} channel has been cleaned out`);
 }
