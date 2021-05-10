b83b6e9041fd3d724082dd5d5610ec04fef7e7c0
Carson <>
2021-05-10 03:31:05 +0000

    respond to new members with a dadjoke in general

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ client.on("message", (msg) => {
   if (msg.content === "!ping") {
     msg.reply("Pong! test");
   }
-  if (msg.content.startsWith("!purge")) {
+  if (msg.content.startsWith("!purge") && msg.member.hasPermission('ADMINISTRATOR') {
     purgeChannel(client, msg);
   }
 });
+
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`!dadjoke`);
+});
+
 
 // Here you can login the bot. It automatically attempts to login the bot
 // with the environment variable you set for your bot token ("DISCORD_TOKEN")
--- purgeChannel.js
+++ purgeChannel.js
@@ -1,12 +1,10 @@
 async function clear(msg, generalChannel) {
-  console.log("deleting messages");
   msg.delete();
   generalChannel.bulkDelete(99, true);
 }
 
 export function purgeChannel(client, msg) {
   const args = msg.content.split(/ +/);
-  console.log("args: ", args);
 
   const generalChannel = msg.guild.channels.cache.find((channel) => {
     return channel.name === args[1];
@@ -14,7 +12,6 @@ export function purgeChannel(client, msg) {
   if (!generalChannel) return;
   const generalObj = client.channels.cache.get(generalChannel.id);
 
-  console.log("generalObj: ", generalObj);
   clear(msg, generalObj);
   msg.author.send(`${generalChannel.name} channel has been cleaned out`);
 }
