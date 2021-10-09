5c7292af1b4f3f2a9933e45f8ac411443b49a575
carwbrown <carwbrown@google.com>
2021-10-09 13:53:14 -0400

    this makes more sense

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ try {
 }
 
 const twitchEventListeners = [];
-
 const castingCaptivatingStreamsId = "714675982442692661";
 
 // https://www.streamweasels.com/support/convert-twitch-username-to-user-id/
@@ -49,15 +48,13 @@ const RAKA = 479927329;
 const VALK = 141728236;
 const KRUSH = 137355398;
 const BRAINER = 59023461;
+const streamers = [RAKA, VALK, KRUSH, BRAINER];
 
 server.listen(3001, async () => {
   try {
     await middleware.markAsReady();
-
-    const streamers = [RAKA, VALK, KRUSH, BRAINER];
-
-    const streamListeners = streamers.map((streamer) => {
-      discordClient.on("ready", async () => {
+    discordClient.on("ready", async () => {
+      const streamListeners = streamers.map((streamer) => {
         const channel = await discordClient.channels.fetch(
           castingCaptivatingStreamsId,
         );
@@ -75,74 +72,10 @@ server.listen(3001, async () => {
         );
 
         twitchEventListeners.push(onlineSub);
+        return onlineSub;
       });
+      await Promise.all(streamListeners);
     });
-
-    await Promise.all(streamListeners);
-    /*
-    discordClient.on("ready", async () => {
-      const channel = await discordClient.channels.fetch("714675982442692661");
-
-      const onlineSub = await middleware.subscribeToStreamOnlineEvents(
-        VALK,
-        (event) => {
-          console.log(`${event.broadcasterDisplayName} just went live!`);
-          channel.send(
-            "CCS broadcasting presents… Valkamer! https://www.twitch.tv/valkamer1366",
-          );
-        },
-      );
-      await onlineSub.stop();
-    });
-
-    discordClient.on("ready", async () => {
-      const channel = await discordClient.channels.fetch("714675982442692661");
-
-      const onlineSub = await middleware.subscribeToStreamOnlineEvents(
-        KRUSH,
-        (event) => {
-          console.log(`${event.broadcasterDisplayName} just went live!`);
-          channel.send(
-            "The smooth stylings of TheKrushinator are live https://www.twitch.tv/thekrushinator",
-          );
-        },
-      );
-      await onlineSub.stop();
-    });
-
-    discordClient.on("ready", async () => {
-      const channel = await discordClient.channels.fetch("714675982442692661");
-
-      const onlineSub = await middleware.subscribeToStreamOnlineEvents(
-        BRAINER,
-        (event) => {
-          console.log(`${event.broadcasterDisplayName} just went live!`);
-          channel.send(
-            "Get in here! Brainer is live! https://www.twitch.tv/brainer1023",
-          );
-        },
-      );
-      await onlineSub.stop();
-    });
-
-    discordClient.on("ready", async () => {
-      const channel = await discordClient.channels.fetch("714675982442692661");
-
-      const onlineSub = await middleware.subscribeToStreamOnlineEvents(
-        RAKA,
-        (event) => {
-          console.log(`${event.broadcasterDisplayName} just went live!`);
-          channel.send(
-            `Hype! ${event.broadcasterDisplayName} is live. "${
-              event.getStream().title || ""
-            }" https://www.twitch.tv/${event.broadcasterName}`,
-          );
-        },
-      );
-
-      twitchEventListeners.push(onlineSub);
-    });
-    */
   } catch (err) {
     console.log("listening error: ", err);
   }
@@ -156,9 +89,11 @@ function shutDown() {
     debug("HTTP server closed");
   });
 
-  await Promise.all(
-    twitchEventListeners.map((twitchEventListener) =>
-      twitchEventListener.stop(),
-    ),
-  );
+  if (twitchEventListeners && twitchEventListeners.length > 0) {
+    await Promise.all(
+      twitchEventListeners.map((twitchEventListener) =>
+        twitchEventListener.stop(),
+      ),
+    );
+  }
 }
